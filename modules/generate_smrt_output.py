6ee8183e02a32a34dacafdce85c29286c0b6326e
Nicolas Leroux <nicolas.leroux@ec.gc.ca>
2024-04-30 14:15:55 +0000

    SMRT: frequency as input and using upper soil temp from SVS2

--- modules/generate_smrt_output.py
+++ modules/generate_smrt_output.py
@@ -42,7 +42,7 @@ def generate_encodings(data):
         encoding[var] = DEFAULT_ENCODING.copy()
     return encoding
                       
-def generate_smrt_output():               
+def generate_smrt_output(freq):               
     '''
     Roughness model: Geometrical Optics Backscatter model
     Permittivity model: static complexe permittivity values to optimize from C-band
@@ -53,18 +53,12 @@ def generate_smrt_output():
     lc_soil = 0.1
     eps = complex(3.5, 0.1)
     mss=2*(sig_soil/lc_soil)**2
-    t_soil = 265.
-    sub = make_soil('geometrical_optics', 
-                    permittivity_model = eps, 
-                    mean_square_slope=mss, 
-                    temperature = t_soil)
 
-              
-
-
+             
     # Read simulation results
     mod = xr.open_dataset(os.path.join(cfg.dir_exp,'output','out_svs2.nc'))
 
+    df_soil = mod['TPSOIL'].to_dataframe() 
     df = mod[['SNODEN_ML','SNOMA_ML','TSNOW_ML','SNODOPT_ML','SNODP']].to_dataframe() 
     # SNODEN_ML: densite des couches
     # SNOMA_ML: SWE des couches
@@ -92,9 +86,16 @@ def generate_smrt_output():
     for tt in times: # just the last time
         d = df.loc[tt].copy()
 
-        
+        d_soil = df_soil.loc[tt]
         d = d[d['SNOMA_ML'] > 0]  # Select only layers with a mass  
-        if len(d) > 0:
+
+        if len(d) > 0: # If there is at least obe layer
+
+            sub = smrt.make_soil('geometrical_optics', 
+                            permittivity_model = eps, 
+                            mean_square_slope=mss, 
+                            temperature = d_soil.iloc[0].values[0])
+
             d['thickness'] = d[['SNODEN_ML','SNOMA_ML']].apply(lambda x : x[1] / x[0], axis = 1) 
             d['SNOSSA_ML'] = d['SNODOPT_ML'].apply(lambda x: 6./(x * DENSITY_OF_ICE) if x>0 else 0) 
             d['corr length'] = d[['SNODEN_ML','SNOSSA_ML']].apply(lambda x: debye_eqn(x[1], x[0]), axis = 1)
@@ -110,7 +111,7 @@ def generate_smrt_output():
             model = make_model("iba", "dort", rtsolver_options = {'error_handling':'nan', 'phase_normalization' : True})
 
 
-            sensor  = sensor_list.active(13e9, 35)
+            sensor  = sensor_list.active(freq, 35)
 
             #run the model
             result = model.run(sensor, snowpack, parallel_computation=False)
